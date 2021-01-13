f7b02427a24cf74340b6a261c1cba53d6170bb55
Nathan <73165547+Nathan@users.noreply.github.com>
2021-01-13 12:25:51 +0100

    Details Gentil personnage

--- groupeL5/src/main/java/be/helha/aemt/navigation/NavigationController.java
+++ groupeL5/src/main/java/be/helha/aemt/navigation/NavigationController.java
@@ -5,17 +5,30 @@ import java.io.Serializable;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 
+import be.helha.aemt.control.EtudiantControl;
+import be.helha.aemt.entities.Etudiant;
+
 @Named
 @SessionScoped
-public class NavigationController implements Serializable{
+public class NavigationController implements Serializable {
 	
-	
+	//public EtudiantControl etudiantC;
 	
 
 	public NavigationController() {
 		
 	}
-
+	/*public String etudiantPrecedent() {
+		int etudpre = getIdEtudiantPrecedent();
+		
+		return"details.xhtml" +"?faces-redirect=true";
+	}
+	public int getIdEtudiantPrecedent() {
+		return etudiantC.getEtudiant().getId() > 0 ? etudiantC.getEtudiant().getId() - 1: 0;
+	}
+	public String etudiantSuivant() {
+		return"details.xhtml" +"?faces-redirect=true";
+	}*/
 	
 	
 	public String goToHome() {
